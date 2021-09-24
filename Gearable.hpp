22af9d4f0e6954a0cfdc8dc96b20c261841d392a
BarTes8 <barbara.tessema@gmail.com>
2021-09-24 13:57:44 +0200

    add ManualGear enum class

--- Gearable.hpp
+++ Gearable.hpp
@@ -1,5 +1,20 @@
 #pragma once
 
+enum class ManualGear : int {
+   reverse = -1,
+   neutral,
+   gear_1st,
+   gear_2nd,
+   gear_3rd,
+   gear_4th,
+   gear_5th,
+   gear_6th,
+   gear_7th,
+   gear_8th,
+   gear_9th,
+   limit = gear_9th
+};
+
 class Gearable {
 public:
    virtual ~Gearable() = default;
